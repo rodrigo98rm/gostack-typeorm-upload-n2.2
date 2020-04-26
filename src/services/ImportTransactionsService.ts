fbf05c2b7f406e297a5e3fd571eadbda8c6150f5
rodrigo98rm <mayerrodrigo98@gmail.com>
2020-04-25 23:56:03 -0300

    Import transactions

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,60 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  fileName: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ fileName }: Request): Promise<Transaction[]> {
+    const createTransactionsService = new CreateTransactionService();
+
+    const csvFilePath = path.resolve(__dirname, '..', '..', 'tmp', fileName);
+
+    const lines = await this.loadCSV(csvFilePath);
+
+    fs.promises.unlink(csvFilePath);
+    const transactions: Transaction[] = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const line of lines) {
+      const [title, type, value, category] = line;
+
+      // eslint-disable-next-line no-await-in-loop
+      const transaction = await createTransactionsService.execute({
+        title,
+        type,
+        value,
+        category,
+      });
+
+      transactions.push(transaction);
+    }
+
+    return transactions;
+  }
+
+  async loadCSV(filePath: string): Promise<any> {
+    const readCSVStream = fs.createReadStream(filePath);
+
+    const parseStream = csvParse({ from_line: 2, ltrim: true, rtrim: true });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const lines: any[] = [];
+
+    parseCSV.on('data', line => {
+      lines.push(line);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    return lines;
   }
 }
 
